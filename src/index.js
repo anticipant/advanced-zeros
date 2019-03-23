0a42db19f363192695b28e923136375f94765475
anticipant <shakhovich.d@yandex.ru>
2019-03-24 00:54:56 +0300

    implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var finalArr = [];
+    var tempObjOfPowerNumber = {};
+    var bottleNeck = {};
+    for (var i = 2; i <= base; i++) {
+        while ((base % i) === 0) {
+
+            if ( tempObjOfPowerNumber[i]) {
+                tempObjOfPowerNumber[i] = tempObjOfPowerNumber[i] + 1;
+            } else {
+                tempObjOfPowerNumber[i] = 1;
+            }
+            base = base / i;
+        }
+    }
+//  console.log(tempObjOfPowerNumber);
+    //разложил на простые множетели
+    for (var j in tempObjOfPowerNumber) {
+        bottleNeck[j] = 0;
+        for (var i = 1, x = 100; i < x; i++) {
+            bottleNeck[j] += Math.floor(number / Math.pow(+j, i));
+        }
+    }
+    // нашел узкие места
+    for (var j in tempObjOfPowerNumber) {
+        finalArr.push(bottleNeck[j] / tempObjOfPowerNumber[j]);
+    }
+    var minNum = Math.min(...finalArr)
+    return Math.floor(minNum);
 }
